294ea031035d0493a05d47c3a785cd966e80e60b
Yannick De Decker <71879638+YannickDeDecker@users.noreply.github.com>
2021-01-16 21:25:00 +0100

    update

--- 1 Form Validator/app.js
+++ 1 Form Validator/app.js
@@ -6,45 +6,45 @@ let passwordConfirmation = document.querySelector('#confirm');
 
 submit.addEventListener('click', formValidation);
 
-function formValidation(event) {
-    event.preventDefault();
-    if (username.value.length < 3 || username.value.lenght === null){
-        console.log(`${username.value} is too short`);
-        username.classList.add('false');
+function formFailure(field){
+    field.classList.add('false');
+}
+
+function formSuccess(field){
+    field.classList.add('right');
+}
+
+function formValidation(e){
+    e.preventDefault();
+    if (username.value.length < 3){
+        formFailure(username);
         document.querySelector('#usernameHelpId').classList.remove('d-none');
     } else {
-        console.log(`${username.value} is OK`)
-        username.classList.add('right');
+        formSuccess(username);
         document.querySelector('#usernameHelpId').classList.add('d-none');
     }
 
     if (email.value.length === null || !email.value.includes('@')){
-        console.log(`${email.value} is not valid`);
-        email.classList.add('false');
+        formFailure(email);
         document.querySelector('#emailHelpId').classList.remove('d-none');
     } else {
-        console.log(`${email.value} is OK`)
-        email.classList.add('right');
+        formSuccess(email);
         document.querySelector('#emailHelpId').classList.add('d-none');
     }
 
-    if (password.value.length < 6 || password.value.lenght === null){
-        console.log(`${password.value} is too short`);
-        password.classList.add('false');
+    if (password.value.length < 6){
+        formFailure(password);
         document.querySelector('#passwordHelpId').classList.remove('d-none');
     } else {
-        console.log(`${password.value} is OK`)
-        password.classList.add('right');
+        formSuccess(password);
         document.querySelector('#passwordHelpId').classList.add('d-none');
     }
 
     if (passwordConfirmation.value !== password.value || passwordConfirmation.value === ''){
-        console.log(`The passwords do not match`);
-        passwordConfirmation.classList.add('false');
+        formFailure(passwordConfirmation);
         document.querySelector('#confirmHelpId').classList.remove('d-none');
     } else {
-        console.log(`Passwords match`)
-        passwordConfirmation.classList.add('right');
+        formSuccess(passwordConfirmation);
         document.querySelector('#confirmHelpId').classList.add('d-none');
     }
 }
